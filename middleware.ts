3c1582e03dc12ecb7e01a34281720ebe2637a450
milovangudelj <milovan.gudelj@gmail.com>
2023-08-04 16:10:12 +0200

    Make sure `/auth` is excluded from middleware runs

--- middleware.ts
+++ middleware.ts
@@ -40,7 +40,7 @@ export const middleware = async (request: NextRequest) => {
   // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // If you have one
   if (
-    ['/fonts', '/images'].some(
+    ['/fonts', '/images', '/auth'].some(
       (value) => pathname.startsWith(value) || pathname.localeCompare(value) === 0
     )
   ) {
