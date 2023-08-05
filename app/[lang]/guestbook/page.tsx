11886bc603ea4ba722de2f97df9be0073454bc93
milovangudelj <milovan.gudelj@gmail.com>
2023-08-05 18:08:45 +0200

    Filter unpublished entries

--- app/[lang]/guestbook/page.tsx
+++ app/[lang]/guestbook/page.tsx
@@ -22,6 +22,7 @@ export default async function GuestbookPage() {
   const { data: entries } = await supabase
     .from('guestbook')
     .select('id, created_at, created_by, body, author_pfp')
+    .filter('is_published', 'eq', true)
     .order('created_at', { ascending: false })
 
   return (
